87e530f2ca430703d8ab157f32535ad309e78d96
Saad Khan <saamoz2012@gmail.com>
2022-05-30 17:48:02 -0400

    Added checks to wait for Google Maps page to load

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import geopy
-from image_tools import filter_bikelanes, crop_image, write_svg
+from image_tools import filter_bikelanes, crop_image
 from maps_web_scraper import screenshot_page
 import os
 
 if __name__ == '__main__':
-    location_name = "paris"
-    do_screenshot = True
-    screenshot_path = screenshot_page(location_name) if do_screenshot else 'temp/ss.png'
+    location_name = "toronto"
+    screenshot_path = screenshot_page(location_name)
     cropped_image = crop_image(screenshot_path)
     filter_bikelanes(cropped_image, f"output/{location_name}_bikelanes.png")
     os.system(f"potrace temp/pbm_image.pbm --svg -o output/{location_name}_bikelanes.svg")
--- maps_web_scraper.py
+++ maps_web_scraper.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-import cv2
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def screenshot_page(location):
@@ -16,18 +16,24 @@ def screenshot_page(location):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
     url = f"https://www.google.com/maps/place/{location}/data=!5m1!1e3"
     driver.get(url)
-    sleep(5)
+    from time import sleep
+    sleep(3)
 
     # hovers over the layer selector then unselects the layers option
-    layer_selector = driver.find_elements(By.XPATH, '/html/body/div[3]/div[9]/div[23]/div[5]/div/div[2]/button')
-    hover = ActionChains(driver).move_to_element(layer_selector[0])
+    layer_selector = driver.find_element(By.XPATH, '/html/body/div[3]/div[9]/div[23]/div[5]/div/div[2]/button')
+    hover = ActionChains(driver).move_to_element(layer_selector)
     hover.perform()
-    driver.find_elements(By.XPATH, '/html/body/div[3]/div[9]/div[23]/div[7]/div/div/div/ul/li[5]/button')[0].click()
-    driver.find_elements(By.XPATH, '/html/body/div[3]/div[9]/div[24]/div/div/div/div[3]/ul/li[2]/button')[0].click()
-    driver.find_elements(By.XPATH, '/html/body/div[3]/div[9]/div[24]/div/div/div/div[1]/header/button')[0].click()
-    driver.find_elements(By.XPATH, '/html/body/div[3]/div[9]/div[9]/div/div/div[2]/button')[0].click()
 
-    # saves the image and opens it in opencv format
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[9]/div[23]/div[7]/div/div/div/ul/li[5]/button'))).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[9]/div[24]/div/div/div/div[3]/ul/li[2]/button'))).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[9]/div[24]/div/div/div/div[1]/header/button'))).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[9]/div[9]/div/div/div[2]/button'))).click()
+
+    # saves the image
     path = 'temp/ss.png'
     driver.save_screenshot(path)
     return path
