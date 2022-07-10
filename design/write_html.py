c2343b8f558f1152ce547095b7095dd1174b8750
Leon <leoncyao@gmail.com>
2022-07-10 12:04:25 -0400

    edited write_html.py script to center text above map images using the html table tag, css needs revision

--- design/write_html.py
+++ design/write_html.py
@@ -1,4 +1,13 @@
 import os
+f = open('header.txt')
+body = f.read().split('\n')
+# plz don't use i if its a name
+for name in sorted(os.listdir("./svg")):
+	body.append("<th>" + name.split('.')[0] + "<img src='svg/" + name + "' alt=''></th>")
 
-for i in sorted(os.listdir("./svg")):
-	print("<div><img src='svg/" + i + "' alt=''></div>")
+body.append('</tr>')
+body.append('</table>')
+body.append('</body>')
+
+g = open('test.html', 'w')
+g.write("".join(body))
