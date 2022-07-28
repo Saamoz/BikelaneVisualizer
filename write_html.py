a1ed56f122847010cad901d5e205c687cbe39ce5
rapsoj <57367002+rapsoj@users.noreply.github.com>
2022-07-28 17:44:49 -0400

    made into grid and added white squares in bg

---
+++ write_html.py
@@ -0,0 +1,20 @@
+import os
+f = open('header.txt')
+body = f.read().split('\n')
+
+i = 0
+body.append('<tr>')
+for name in sorted(os.listdir("./svg")):
+	body.append("<th>" + name.split('.')[0] + "<img src='svg/" + name + "' alt=''></th>")
+
+	if i % 7 == 0:
+		body.append('</tr><tr>')
+
+	i = i + 1
+
+body.append('</tr>')
+body.append('</table>')
+body.append('</body>')
+
+g = open('test.html', 'w')
+g.write("".join(body))
