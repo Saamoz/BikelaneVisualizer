680344249b6518684b3d9c69893eb5383b4db02d
Saad Khan <saamoz2012@gmail.com>
2022-08-11 17:57:40 -0500

    Added canadian cities

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from write_html import make_webpage
 import os
 import csv
 
+image_output_folder_name = "output"
+
 
 def get_city_bikelanes(city_name):
     city = city_name.split(',')[0]
-    screenshot_path = screenshot_page(city)
+    screenshot_path = screenshot_page(city_name)
     cropped_image = crop_image(screenshot_path)
-    filter_bikelanes(cropped_image, f'output/png/{city_name} Bikelanes.png')
-    os.system(f'potrace temp/pbm_image.pbm --svg -o "output/svg/{city_name} Bikelanes.svg"')
+    filter_bikelanes(cropped_image, f'{image_output_folder_name}/png/{city_name} Bikelanes.png')
+    os.system(f'potrace temp/pbm_image.pbm --svg -o "{image_output_folder_name}/svg/{city_name} Bikelanes.svg"')
 
 
 if __name__ == '__main__':
-    city_list_file = "cities.csv"
+    city_list_file = "all_cities.csv"
     with open(city_list_file, 'r', encoding='utf-8') as csvfile:
         datareader = csv.reader(csvfile)
         datareader.__next__()
-        start_name = "Austin"
+        start_name = ""
         do = False
         for row in datareader:
             city_name = ", ".join(row)
@@ -28,4 +30,3 @@ if __name__ == '__main__':
             if do:
                 get_city_bikelanes(city_name)
     make_webpage('output/svg')
-
